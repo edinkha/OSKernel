fa789761712f87aecfa2fe0997bba845d167a27f
Nathan Woltman <nwoltman@outlook.com>
2014-03-16 20:05:25 -0400

    Improved KCD (hopefully) - Made the array of registered commands global (because of what comes next) - Made a function that, when given a string that may be a command line, returns the id of the process registered with that command, or RTX_ERR if there is no process registered with the command - Also took out the unnecessary __disable_irq() lines from timer.c

--- Code/MAIN/src/sys_proc.c
+++ Code/MAIN/src/sys_proc.c
@@ -37,6 +37,16 @@ MSG_BUF* message_to_send;
 PCB* unswitched_proc;
 PCB* old_proc;
 
+// KCD command structure
+typedef struct cmd 
+{ 
+	char cmd_id[4];  /* command identifier  */
+	int reg_proc_id; /* process id of registered process */
+} CMD;
+
+CMD registered_commands[10]; // Array of registered commands for KCD
+int num_reg_commands = 0;    // Number of currently registered commands
+
 #ifdef DEBUG_HK
 /**
  * @brief: Print all of the PCB process_ids in the priority queue, along with their priorities
@@ -251,110 +261,106 @@ void proc_wall_clock()
 	}
 }
 
-void KCD(void)
+/**
+ * Gets the PID of the process that registered the input command
+ * @returns The PID of the process that registered the input command,
+ *          or if there is no process registered with the command, returns RTX_ERR
+ */
+int get_command_proc_id(const char input_line[50])
 {
-	typedef struct cmd 
-	{ 
-		char cmd_id[2];	      /* command identifier  */
-		U32 reg_proc_id;			/* process id of registered process */
-	} CMD;
-	
-	MSG_BUF* received_message;
+	int i;
+
+	if (input_line[0] != '%') {
+		return RTX_ERR;
+	}
+
+	for (i = 0; i < num_reg_commands; ++i) {
+		int j = 1;
+		do {
+			if (registered_commands[i].cmd_id[j] == '\0' && (input_line[j] == '\0' || input_line[j] == ' ')) {
+				return registered_commands[i].reg_proc_id;
+			}
+		} while (input_line[j] == registered_commands[i].cmd_id[j]);
+	}
+
+	return RTX_ERR;
+}
+
+void KCD(void)
+{	
+	MSG_BUF* message_received;
 	MSG_BUF* message_to_send;
-	char user_input_str[1];
-	char* user_input_str_p = user_input_str;
-	CMD registered_commands[10];
-	U32 current_num_commands = 0;
+	char str_user_input[50] = "";
+	int idx_user_input = 0;
+	int command_line_received = 0;
 	int sender_id;
-	char to_parse[1];
-	char* to_parse_p = to_parse;
 	
-	while(1) {
-		// grab the message from the KCD proc message queue
-		received_message = (MSG_BUF*)receive_message(&sender_id);
-		if (received_message->mtype == USER_INPUT) {
-			if (received_message->mtext[0] == '\r') {
+	while (1) {
+		// Receive a message
+		message_received = (MSG_BUF*)receive_message(&sender_id);
+
+		if (message_received->mtype == KCD_REG) { // Register a command with KCD
+			if (message_received->mtext[0] != '%' || message_received->mtext[1] == '\0') {
 				message_to_send = (MSG_BUF*)request_memory_block();
 				message_to_send->mtype = CRT_DISPLAY;
-				message_to_send->mtext[0] = '\r';
-				message_to_send->mtext[1] = '\n';
-				message_to_send->mtext[2] = '\0';
-				send_message(PID_CRT, (void*)message_to_send);
-				user_input_str_p = user_input_str;
-			} else if (received_message->mtext[0] == '\n') {
-				// no-op
-			} else {
-				*user_input_str_p = received_message->mtext[0];
-				message_to_send = (MSG_BUF*)request_memory_block();
-				message_to_send->mtype = CRT_DISPLAY;
-				message_to_send->mtext[0] = *user_input_str_p;
-				message_to_send->mtext[1] = '\0';
-				send_message(PID_CRT, (void*)message_to_send);
-				user_input_str_p++;
+				strcpy(message_to_send->mtext, "ERROR: Commands must begin with the %% character, followed by at least one letter.\n\r");
+				send_message(PID_CRT, message_to_send);
 			}
-		}
-		else if (received_message->mtype == KCD_REG) {
-			int i = 1;
-			if (received_message->mtext[0] != '%') {	
-				char error_str[] = "KCD Registration Commands must begin with the % character.\n\r";
-				message_to_send = (MSG_BUF*)request_memory_block();
-				message_to_send->mtype = CRT_DISPLAY;
-				strcpy(message_to_send->mtext, error_str);
-				send_message(PID_CRT, (void*)message_to_send);
-			}
-			while (received_message->mtext[i] != '\0') {
-				registered_commands[current_num_commands].cmd_id[i-1] = received_message->mtext[i];
-				i++;
-			}
-			// null-terminate the id
-			registered_commands[current_num_commands].cmd_id[i-1] = '\0';
-			registered_commands[current_num_commands].reg_proc_id = sender_id;
-			current_num_commands++;
-		}
-		else if (received_message->mtype != KCD_REG) {
-			int is_match = 1, num_com;
-			if (received_message->mtype == USER_INPUT) {
-				to_parse_p = user_input_str;
-			} else {
-				int i = 0;
-				to_parse_p = &received_message->mtext[0];
-				message_to_send = (MSG_BUF*)request_memory_block();
-				message_to_send->mtype = CRT_DISPLAY;
-				while (*to_parse_p != '\0') {
-					message_to_send->mtext[i] = *to_parse_p;
-					to_parse_p++;
+			else {
+				int i = 1;
+				while (message_received->mtext[i] != '\0') {
+					registered_commands[num_reg_commands].cmd_id[i-1] = message_received->mtext[i];
 					i++;
 				}
-				message_to_send->mtext[i] = '\0';
-				send_message(PID_CRT, (void*)message_to_send);
-				to_parse_p = &received_message->mtext[0];
-			}
-			for (num_com = 0; num_com < 10; num_com++) {
-				int c = 0;
-				while (*to_parse_p != '\0') {
-					if (registered_commands[num_com].cmd_id[c] != *to_parse_p) {
-						is_match = 0;
-						break;
-					}
-					c++;
-					to_parse_p++;
-				}
-				
-				if (is_match && *to_parse_p == registered_commands[num_com].cmd_id[c]) {
-					int j = 0;
-					char* index = to_parse;
-					message_to_send = (MSG_BUF*)request_memory_block();
-					message_to_send->mtype = DEFAULT;
-					while (*index != '\0') {
-						message_to_send->mtext[j] = *index;
-						index++;
-						j++;
-					}
-					message_to_send->mtext[j] = '\0';
-					send_message(registered_commands[num_com].reg_proc_id, (void*)message_to_send);
-				}
+				registered_commands[num_reg_commands].cmd_id[i-1] = '\0';      // Null-terminate the id
+				registered_commands[num_reg_commands].reg_proc_id = sender_id; // Set the registered proc id
+				num_reg_commands++; // Increment the number of registered commands
 			}
 		}
-		release_memory_block((void*)received_message);
+		else if (message_received->mtype == USER_INPUT) { // User input a character
+			// Build message to send to CRT to output the input character
+			message_to_send = (MSG_BUF*)request_memory_block();
+			message_to_send->mtype = CRT_DISPLAY;
+			message_to_send->mtext[0] = message_received->mtext[0];
+
+			if (message_received->mtext[0] == '\r') { // The user pressed ENTER
+				message_to_send->mtext[1] = '\n';
+				message_to_send->mtext[2] = '\0';
+				
+				// A full command line has been received, so put the input string into the received message's text
+				// and set the flag so that it may be forwarded
+				strcpy(message_received->mtext, str_user_input);
+				command_line_received = 1;
+
+				// Reset the user input string and input string index
+				str_user_input[0] = '\0';
+				idx_user_input = 0;
+			}
+			else {
+				message_to_send->mtext[1] = '\0';
+
+				// Add the character to the user input string
+				str_user_input[idx_user_input] = message_received->mtext[0];
+				str_user_input[++idx_user_input] = '\0';
+			}
+
+			send_message(PID_CRT, message_to_send); // Send message to CRT
+		}
+		else { // We have received a normal message from a test process that contains a command line
+			command_line_received = 1;
+		}
+
+		if (command_line_received) { // We have received a full command line and its text is located in message_received->mtext
+			// Retreive the id of the process that registered the command
+			int reg_id = get_command_proc_id(message_received->mtext);
+			command_line_received = 0; // Reset the flag
+			if (reg_id != RTX_ERR) {
+				// The line was a valid command and we have retreived the id of the process registered for that command
+				send_message(reg_id, message_received); // Forward the message to the registered process
+				continue; // Go back to the start of the loop (so that we don't release the memory of the forwarded message)
+			}
+		}
+
+		release_memory_block(message_received);
 	}
 }
