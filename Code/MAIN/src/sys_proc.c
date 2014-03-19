8e8fe7b1f1e0393370e017349d720683a16e5893
Justin Gagne <justingagne5@gmail.com>
2014-03-18 21:29:02 -0400

    Changed the order of procs in sys_proc
    Just to match up better with how we define them in k_rtx.

--- Code/MAIN/src/sys_proc.c
+++ Code/MAIN/src/sys_proc.c
@@ -43,107 +43,118 @@ typedef struct cmd
 CMD registered_commands[10]; // Array of registered commands for KCD
 int num_reg_commands = 0;    // Number of currently registered commands
 
-#ifdef DEBUG_HK
 /**
- * @brief: Print all of the PCB process_ids in the priority queue, along with their priorities
+ * Gets the PID of the process that registered the input command
+ * @returns The PID of the process that registered the input command,
+ *          or if there is no process registered with the command, returns RTX_ERR
  */
-void print(PriorityQueue* pqueue)
+int get_command_proc_id(const char input_line[50])
 {
 	int i;
-	QNode* cur_node;
-	assert(pqueue != NULL);
-	for (i = 0; i < NUM_PRIORITIES; i++) {
-		cur_node = pqueue->queues[i].first;
-		while (cur_node != NULL) {
-			printf("Process ID = %d\n\r", ((PCB*)cur_node)->m_pid);
-			printf("Process Priority = %d\n\r", ((PCB*)cur_node)->m_priority);
-			cur_node = cur_node->next;
-		}
+
+	if (input_line[0] != '%') {
+		return RTX_ERR;
 	}
+
+	for (i = 0; i < num_reg_commands; ++i) {
+		int j = 0;
+		do {
+			j++;
+			if (registered_commands[i].cmd_id[j-1] == '\0' && (input_line[j] == '\0' || input_line[j] == ' ')) {
+				return registered_commands[i].reg_proc_id;
+			}
+		} while (input_line[j] == registered_commands[i].cmd_id[j-1]);
+	}
+
+	return RTX_ERR;
 }
-#endif
 
-// UART initialized in uart_irq.c
-void UART_IPROC(void)
-{
-	MSG_BUF* received_message;
+void KCD(void)
+{	
+	MSG_BUF* message_received;
 	MSG_BUF* message_to_send;
+	char str_user_input[50] = "";
+	int idx_user_input = 0;
+	int command_line_received = 0;
+	int sender_id;
+	
+	while (1) {
+		// Receive a message
+		message_received = (MSG_BUF*)receive_message(&sender_id);
 
-	// Save the previously running proccess and set the current process to this i-proc 
-	PCB* old_proc = gp_current_process;
-	gp_current_process = get_proc_by_pid(PID_UART_IPROC);
+		if (message_received->mtype == KCD_REG) { // Register a command with KCD
+			if (message_received->mtext[0] != '%' || message_received->mtext[1] == '\0') {
+				message_to_send = (MSG_BUF*)request_memory_block();
+				message_to_send->mtype = CRT_DISPLAY;
+				strcpy(message_to_send->mtext, "ERROR: Commands must begin with the %% character, followed by at least one letter.\n\r");
+				send_message(PID_CRT, message_to_send);
+			}
+			else {
+				int i = 1;
+				while (message_received->mtext[i] != '\0') {
+					registered_commands[num_reg_commands].cmd_id[i-1] = message_received->mtext[i];
+					i++;
+				}
+				registered_commands[num_reg_commands].cmd_id[i-1] = '\0';      // Null-terminate the id
+				registered_commands[num_reg_commands].reg_proc_id = sender_id; // Set the registered proc id
+				num_reg_commands++; // Increment the number of registered commands
+			}
+		}
+		else if (message_received->mtype == USER_INPUT) { // User input a character
+			// Build message to send to CRT to output the input character
+			message_to_send = (MSG_BUF*)request_memory_block();
+			message_to_send->mtype = CRT_DISPLAY;
+			message_to_send->mtext[0] = message_received->mtext[0];
 
-#ifdef DEBUG_0
-	uart1_put_string("Entering UART i-proc\n\r");
-#endif // DEBUG_0
-	
-	/* Reading IIR automatically acknowledges the interrupt */
-	IIR_IntId = (pUart->IIR) >> 1 ; // skip pending bit in IIR 
-	
-	if (IIR_IntId & IIR_RDA) { // Receive Data Available
-		g_switch_flag = 1;
-		/* read UART. Read RBR will clear the interrupt */
-		g_char_in = pUart->RBR;
-		
-	#ifdef DEBUG_0
-		uart1_put_string("Reading a char = ");
-		uart1_put_char(g_char_in);
-		uart1_put_string("\r\n");
-	#endif // DEBUG_0
-		
-	#ifdef DEBUG_HK
-		if (g_char_in == '!') {
-			uart1_put_string("! hotkey entered - printing processes on ready queue\n\r");
-			print(ready_pq);
+			if (message_received->mtext[0] == '\r') { // The user pressed ENTER
+				message_to_send->mtext[1] = '\n';
+				message_to_send->mtext[2] = '\0';
+				
+				// A full command line has been received, so put the input string into the received message's text
+				// and set the flag so that it may be forwarded
+				strcpy(message_received->mtext, str_user_input);
+				command_line_received = 1;
+
+				// Reset the user input string and input string index
+				str_user_input[0] = '\0';
+				idx_user_input = 0;
+			}
+			else {
+				message_to_send->mtext[1] = '\0';
+
+				if (message_received->mtext[0] == '\b') { // The user pressed BACKSPACE
+					if (idx_user_input > 0) {
+						// Subtract a character from the user input string
+						str_user_input[--idx_user_input] = '\0';
+					}
+				}
+				else {
+					// Add the character to the user input string
+					str_user_input[idx_user_input] = message_received->mtext[0];
+					str_user_input[++idx_user_input] = '\0';
+				}
+			}
+
+			send_message(PID_CRT, message_to_send); // Send message to CRT
 		}
-		else if (g_char_in == '@') {
-			uart1_put_string("@ hotkey entered - printing processes on blocked on memory queue\n\r");
-			print(blocked_memory_pq);
+		else { // We have received a normal message from a test process that contains a command line
+			command_line_received = 1;
 		}
-		else if (g_char_in == '#') {
-			uart1_put_string("# hotkey entered - printing processes on blocked on receive queue\n\r");
-			print(blocked_waiting_pq);
+
+		if (command_line_received) { // We have received a full command line and its text is located in message_received->mtext
+			// Retreive the id of the process that registered the command
+			int reg_id = get_command_proc_id(message_received->mtext);
+			command_line_received = 0; // Reset the flag
+			if (reg_id != RTX_ERR) {
+				// The line was a valid command and we have retreived the id of the process registered for that command
+				message_received->mtype = COMMAND;
+				send_message(reg_id, message_received); // Forward the message to the registered process
+				continue; // Go back to the start of the loop (so that we don't release the memory of the forwarded message)
+			}
 		}
-	#endif // DEBUG_HK
-		
-		// send char to KCD, which will handle parsing and send each character to CRT for printing
-		message_to_send = (MSG_BUF*)ki_request_memory_block();
-		if (message_to_send) {
-			message_to_send->mtype = USER_INPUT;
-			message_to_send->mtext[0] = g_char_in;
-			message_to_send->mtext[1] = '\0';
-			k_send_message(PID_KCD, (void*)message_to_send);
-		}
+
+		release_memory_block(message_received);
 	}
-	else if (IIR_IntId & IIR_THRE) {
-		g_switch_flag = 0;
-		/* THRE Interrupt, transmit holding register becomes empty */
-		received_message = (MSG_BUF*)ki_receive_message((int*)0);
-		gp_buffer = received_message->mtext;
-		while (*gp_buffer != '\0' ) {
-			g_char_out = *gp_buffer;
-		#ifdef DEBUG_0
-			printf("Writing a char = %c \n\r", g_char_out);
-		#endif // DEBUG_0
-			pUart->THR = g_char_out;
-			gp_buffer++;
-		}
-	#ifdef DEBUG_0
-		uart1_put_string("Finish writing. Turning off IER_THRE\n\r");
-	#endif // DEBUG_0
-		k_release_memory_block((void*)received_message);
-		pUart->IER ^= IER_THRE; // toggle the IER_THRE bit 
-		//pUart->THR = '\0';
-		gp_buffer = g_buffer;		
-	}
-	else {  /* not implemented yet */
-	#ifdef DEBUG_0
-		uart1_put_string("Should not get here!\n\r");
-	#endif // DEBUG_0
-	}
-	
-	//Restore the current process
-	gp_current_process = old_proc;
 }
 
 /**
@@ -279,116 +290,105 @@ void proc_wall_clock()
 	}
 }
 
+#ifdef DEBUG_HK
 /**
- * Gets the PID of the process that registered the input command
- * @returns The PID of the process that registered the input command,
- *          or if there is no process registered with the command, returns RTX_ERR
+ * @brief: Print all of the PCB process_ids in the priority queue, along with their priorities
  */
-int get_command_proc_id(const char input_line[50])
+void print(PriorityQueue* pqueue)
 {
 	int i;
-
-	if (input_line[0] != '%') {
-		return RTX_ERR;
+	QNode* cur_node;
+	assert(pqueue != NULL);
+	for (i = 0; i < NUM_PRIORITIES; i++) {
+		cur_node = pqueue->queues[i].first;
+		while (cur_node != NULL) {
+			printf("Process ID = %d\n\r", ((PCB*)cur_node)->m_pid);
+			printf("Process Priority = %d\n\r", ((PCB*)cur_node)->m_priority);
+			cur_node = cur_node->next;
+		}
 	}
-
-	for (i = 0; i < num_reg_commands; ++i) {
-		int j = 0;
-		do {
-			j++;
-			if (registered_commands[i].cmd_id[j-1] == '\0' && (input_line[j] == '\0' || input_line[j] == ' ')) {
-				return registered_commands[i].reg_proc_id;
-			}
-		} while (input_line[j] == registered_commands[i].cmd_id[j-1]);
-	}
-
-	return RTX_ERR;
 }
+#endif
 
-void KCD(void)
-{	
-	MSG_BUF* message_received;
+// UART initialized in uart_irq.c
+void UART_IPROC(void)
+{
+	MSG_BUF* received_message;
 	MSG_BUF* message_to_send;
-	char str_user_input[50] = "";
-	int idx_user_input = 0;
-	int command_line_received = 0;
-	int sender_id;
+
+	// Save the previously running proccess and set the current process to this i-proc 
+	PCB* old_proc = gp_current_process;
+	gp_current_process = get_proc_by_pid(PID_UART_IPROC);
+
+#ifdef DEBUG_0
+	uart1_put_string("Entering UART i-proc\n\r");
+#endif // DEBUG_0
 	
-	while (1) {
-		// Receive a message
-		message_received = (MSG_BUF*)receive_message(&sender_id);
-
-		if (message_received->mtype == KCD_REG) { // Register a command with KCD
-			if (message_received->mtext[0] != '%' || message_received->mtext[1] == '\0') {
-				message_to_send = (MSG_BUF*)request_memory_block();
-				message_to_send->mtype = CRT_DISPLAY;
-				strcpy(message_to_send->mtext, "ERROR: Commands must begin with the %% character, followed by at least one letter.\n\r");
-				send_message(PID_CRT, message_to_send);
-			}
-			else {
-				int i = 1;
-				while (message_received->mtext[i] != '\0') {
-					registered_commands[num_reg_commands].cmd_id[i-1] = message_received->mtext[i];
-					i++;
-				}
-				registered_commands[num_reg_commands].cmd_id[i-1] = '\0';      // Null-terminate the id
-				registered_commands[num_reg_commands].reg_proc_id = sender_id; // Set the registered proc id
-				num_reg_commands++; // Increment the number of registered commands
-			}
+	/* Reading IIR automatically acknowledges the interrupt */
+	IIR_IntId = (pUart->IIR) >> 1 ; // skip pending bit in IIR 
+	
+	if (IIR_IntId & IIR_RDA) { // Receive Data Available
+		g_switch_flag = 1;
+		/* read UART. Read RBR will clear the interrupt */
+		g_char_in = pUart->RBR;
+		
+	#ifdef DEBUG_0
+		uart1_put_string("Reading a char = ");
+		uart1_put_char(g_char_in);
+		uart1_put_string("\r\n");
+	#endif // DEBUG_0
+		
+	#ifdef DEBUG_HK
+		if (g_char_in == '!') {
+			uart1_put_string("! hotkey entered - printing processes on ready queue\n\r");
+			print(ready_pq);
 		}
-		else if (message_received->mtype == USER_INPUT) { // User input a character
-			// Build message to send to CRT to output the input character
-			message_to_send = (MSG_BUF*)request_memory_block();
-			message_to_send->mtype = CRT_DISPLAY;
-			message_to_send->mtext[0] = message_received->mtext[0];
-
-			if (message_received->mtext[0] == '\r') { // The user pressed ENTER
-				message_to_send->mtext[1] = '\n';
-				message_to_send->mtext[2] = '\0';
-				
-				// A full command line has been received, so put the input string into the received message's text
-				// and set the flag so that it may be forwarded
-				strcpy(message_received->mtext, str_user_input);
-				command_line_received = 1;
-
-				// Reset the user input string and input string index
-				str_user_input[0] = '\0';
-				idx_user_input = 0;
-			}
-			else {
-				message_to_send->mtext[1] = '\0';
-
-				if (message_received->mtext[0] == '\b') { // The user pressed BACKSPACE
-					if (idx_user_input > 0) {
-						// Subtract a character from the user input string
-						str_user_input[--idx_user_input] = '\0';
-					}
-				}
-				else {
-					// Add the character to the user input string
-					str_user_input[idx_user_input] = message_received->mtext[0];
-					str_user_input[++idx_user_input] = '\0';
-				}
-			}
-
-			send_message(PID_CRT, message_to_send); // Send message to CRT
+		else if (g_char_in == '@') {
+			uart1_put_string("@ hotkey entered - printing processes on blocked on memory queue\n\r");
+			print(blocked_memory_pq);
 		}
-		else { // We have received a normal message from a test process that contains a command line
-			command_line_received = 1;
+		else if (g_char_in == '#') {
+			uart1_put_string("# hotkey entered - printing processes on blocked on receive queue\n\r");
+			print(blocked_waiting_pq);
 		}
-
-		if (command_line_received) { // We have received a full command line and its text is located in message_received->mtext
-			// Retreive the id of the process that registered the command
-			int reg_id = get_command_proc_id(message_received->mtext);
-			command_line_received = 0; // Reset the flag
-			if (reg_id != RTX_ERR) {
-				// The line was a valid command and we have retreived the id of the process registered for that command
-				message_received->mtype = COMMAND;
-				send_message(reg_id, message_received); // Forward the message to the registered process
-				continue; // Go back to the start of the loop (so that we don't release the memory of the forwarded message)
-			}
+	#endif // DEBUG_HK
+		
+		// send char to KCD, which will handle parsing and send each character to CRT for printing
+		message_to_send = (MSG_BUF*)ki_request_memory_block();
+		if (message_to_send) {
+			message_to_send->mtype = USER_INPUT;
+			message_to_send->mtext[0] = g_char_in;
+			message_to_send->mtext[1] = '\0';
+			k_send_message(PID_KCD, (void*)message_to_send);
 		}
-
-		release_memory_block(message_received);
 	}
+	else if (IIR_IntId & IIR_THRE) {
+		g_switch_flag = 0;
+		/* THRE Interrupt, transmit holding register becomes empty */
+		received_message = (MSG_BUF*)ki_receive_message((int*)0);
+		gp_buffer = received_message->mtext;
+		while (*gp_buffer != '\0' ) {
+			g_char_out = *gp_buffer;
+		#ifdef DEBUG_0
+			printf("Writing a char = %c \n\r", g_char_out);
+		#endif // DEBUG_0
+			pUart->THR = g_char_out;
+			gp_buffer++;
+		}
+	#ifdef DEBUG_0
+		uart1_put_string("Finish writing. Turning off IER_THRE\n\r");
+	#endif // DEBUG_0
+		k_release_memory_block((void*)received_message);
+		pUart->IER ^= IER_THRE; // toggle the IER_THRE bit 
+		//pUart->THR = '\0';
+		gp_buffer = g_buffer;		
+	}
+	else {  /* not implemented yet */
+	#ifdef DEBUG_0
+		uart1_put_string("Should not get here!\n\r");
+	#endif // DEBUG_0
+	}
+	
+	//Restore the current process
+	gp_current_process = old_proc;
 }
