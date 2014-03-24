fde8422727bccc289b0970f3ee50f6d321fbffba
unknown <jjgagne@ECE-RTOS16.NEXUS.UWATERLOO.CA>
2014-03-24 00:25:41 -0400

    Changed mtext to 100 chars for all declarations

--- Code/MAIN/src/rtx.h
+++ Code/MAIN/src/rtx.h
@@ -60,7 +60,7 @@ typedef struct proc_init
 typedef struct msgbuf
 {
 	int mtype;              /* user defined message type */
-	char mtext[1];          /* body of the message */
+	char mtext[100];          /* body of the message */
 } MSG_BUF;
 
 /* ----- RTX User API ----- */
